b7e8a0a871a350aeb4e0b69ed4b20c1db42acfa7
tbone255 <taha.amir13971@gmail.com>
2016-11-18 20:59:59 -0500

    excel

--- linker.py
+++ linker.py
@@ -1,17 +1,17 @@
+#for ABCBIRDS
 import openpyxl
 import os
-
-
 class Linker:
     '''
     This class was made to grab links from an excell sheet,
     and to add notes to it like the new link or the status
     '''
-
-    def __init__(self):  # wb is the excel workbook name
-        self.wb = openpyxl.load_workbook('data.xlsx')
+    def __init__(self): #wb is the excel workbook name
+        self.wb= openpyxl.load_workbook('links.xlsx')
+        #print(self.fileName)
         self.sheet = self.wb.get_sheet_by_name('Sheet1')
-
+        #print('------"%s" has been selected------' % (self.sheet.title))
+        #print(self.sheet['A1045'].value)
         self.rowNum = 1;
 
     def sheetTitle(self):
@@ -25,3 +25,14 @@ class Linker:
         self.sheet['D' + str(self.rowNum)].value = note;
         return note;
 
+    def save(self):
+        self.wb.save('links-updated.xlsx')
+
+    def length(self):
+        self.startRow = self.rowNum;
+        self.current = self.getNextLink()
+        while not self.current is None:
+            self.current = self.getNextLink()
+        self.temp2 = self.rowNum
+        self.rowNum = self.startRow
+        return self.temp2 - 1
