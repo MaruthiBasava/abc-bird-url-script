c42a7167432c710b959727f5da025529bd993a0a
tbone255 <taha.amir13971@gmail.com>
2016-11-21 19:29:26 -0500

    Update entry.py

--- entry.py
+++ entry.py
@@ -2,11 +2,12 @@ from bs4 import BeautifulSoup
 import requests
 from linker import Linker
 import re
+from urllib.request import Request, urlopen
 from urllib.error import HTTPError
+from unidecode import unidecode
 
 
 link = Linker()
-
 
 def format_url():
     base_url = "https://web.archive.org/web/"
@@ -14,36 +15,34 @@ def format_url():
     return base_url + date + link.getNextLink()
 
 
-def format_title_to_url(result):
-    a = result.text
-    b = remove_non_ascii(a)
-    c = re.sub('\s+', ' ', b).strip()
-    d = c.replace(" ", "-")
-    return d.replace(" ", "")
-
-
-def get_parsed_title(url): 
+def get_parsed_title(url): #gets title and parses it
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
     result = soup.find('span', class_='H1')
 
-    if result is None:
-        result = soup.find('h1', {"align": "center"})
-        if result is None:
+    if result == None:
+        result = soup.find('h1', {"align":"center"})
+        if result == None:
             return "no-wayback"
 
-    return format_title_to_url(result)
+    a = result.text
+    b = _removeNonAscii(a)
+    c = re.sub('\s+', ' ', b).strip()
+    d = c.replace(" ", "-")
+    return d.replace(" ", "")
 
-
-def remove_non_ascii(s):
-    return "".join(i for i in s if (ord(i) == 32 or ord(i) >= 65 <= 90 or ord(i) >= 97 <= 122 or ord(i) >= 48 <= 57))
-
+def _removeNonAscii(s):
+    return "".join(i for i in s if (ord(i) == 32 or ord(i) >= 65 and ord(i) <= 90 or ord(i) >= 97 and ord(i) <= 122 or 48 <= ord(i) <= 57 or ord(i) == 45 ))
 
 def return_working_title(url):
-    try:
-        page = requests.get(url)
+    hdr = {'User-agent': 'Mozilla/5.0'}
+    req = Request(url, headers=hdr)
 
-        soup = BeautifulSoup(page.text, 'html.parser')
+    try:
+        page = urlopen(req).read
+        ppage = requests.get(url)
+
+        soup = BeautifulSoup(ppage.text, 'html.parser')
 
         if "404 " in soup.text:
             return "not in new site"
@@ -52,10 +51,9 @@ def return_working_title(url):
 
     return url
 
-
-def add_all_links(a,b):
+def add_all_links(a, b): #will add the link or note to the sheet
     link.rowNum = a
-    for i in range(a,b):
+    for i in range(a, b):
         a = get_parsed_title(format_url())
         current_link = return_working_title("https://abcbirds.org/" + a)
         print(str(i) + " " + a + "  \n  " + "https://abcbirds.org/" + a)
@@ -68,8 +66,10 @@ def add_all_links(a,b):
 
     link.save()
 
+
+
 try:
-    add_all_links(0,link.length())
+    add_all_links(30, 35)
 except:
     link.save()
     print("FAILED")
